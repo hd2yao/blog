82e4bc93fa4cc998bf0ef0fcd284b645cf1eb3b0
hd2yao <1054290355@qq.com>
2023-06-26 15:16:53 +0800

    test

--- internal/routers/api/v1/article.go
+++ internal/routers/api/v1/article.go
@@ -1,6 +1,11 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+    "github.com/gin-gonic/gin"
+
+    "blog/pkg/app"
+    "blog/pkg/err_code"
+)
 
 func NewArticle() Article {
     return Article{}
@@ -8,7 +13,10 @@ func NewArticle() Article {
 
 type Article struct{}
 
-func (a Article) Get(c *gin.Context)    {}
+func (a Article) Get(c *gin.Context) {
+    app.NewResponse(c).ToErrorResponse(err_code.ServerError)
+    return
+}
 func (a Article) List(c *gin.Context)   {}
 func (a Article) Create(c *gin.Context) {}
 func (a Article) Update(c *gin.Context) {}
