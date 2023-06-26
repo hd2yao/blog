54f59048543f0205de00750758d500fe182bd631
hd2yao <1054290355@qq.com>
2023-06-26 11:11:14 +0800

    upgrade:main

--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
+    "blog/internal/routers"
     "net/http"
+    "time"
 )
 
 func main() {
-    r := gin.Default()
-    r.GET("/ping", func(context *gin.Context) {
-        context.JSON(http.StatusOK, gin.H{"message": "pong"})
-    })
-    r.Run()
+    // 1.0.0
+    //r := gin.Default()
+    //r.GET("/ping", func(context *gin.Context) {
+    //    context.JSON(http.StatusOK, gin.H{"message": "pong"})
+    //})
+    //r.Run()
+
+    // 2.0.0
+    router := routers.NewPouter()
+    s := &http.Server{
+        Addr:           ":8080",
+        Handler:        router,
+        ReadTimeout:    10 * time.Second,
+        WriteTimeout:   10 * time.Second,
+        MaxHeaderBytes: 1 << 20,
+    }
+    s.ListenAndServe()
 }
