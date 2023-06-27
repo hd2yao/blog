93501f14ae521f0c0696defc9645d76d6bbe1997
hd2yao <1054290355@qq.com>
2023-06-27 16:58:08 +0800

    feat:api validator

--- internal/routers/api/v1/tag.go
+++ internal/routers/api/v1/tag.go
@@ -2,6 +2,9 @@ package v1
 
 import (
     "github.com/gin-gonic/gin"
+    "github.com/hd2yao/blog/global"
+    "github.com/hd2yao/blog/pkg/app"
+    "github.com/hd2yao/blog/pkg/err_code"
 )
 
 func NewTag() Tag {
@@ -22,7 +25,21 @@ func (t Tag) Get(c *gin.Context) {}
 // @Failure 400 {object} err_code.Error "请求错误"
 // @Failure 500 {object} err_code.Error "内部错误"
 // @Router /api/v1/tags [get]
-func (t Tag) List(c *gin.Context) {}
+func (t Tag) List(c *gin.Context) {
+    param := struct {
+        Name  string `form:"name" binding:"max=100"`
+        State uint8  `form:"state,default=1" binding:"oneof=0 1"`
+    }{}
+    response := app.NewResponse(c)
+    valid, errs := app.BindAndValid(c, &param)
+    if !valid {
+        global.Logger.Errorf("app.BindAndValid errs: %v", errs)
+        response.ToErrorResponse(err_code.InvalidParams.WithDetails(errs.Errors()...))
+        return
+    }
+    response.ToResponse(gin.H{})
+    return
+}
 
 // @Summary 新增标签
 // @Produce  json
