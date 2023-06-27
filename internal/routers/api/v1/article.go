d7ba9e5b52af4e280d4989bfcfaa801f7db317cb
hd2yao <1054290355@qq.com>
2023-06-27 13:25:59 +0800

    feat:add swagger model

--- internal/routers/api/v1/article.go
+++ internal/routers/api/v1/article.go
@@ -27,7 +27,7 @@ func (a Article) Get(c *gin.Context) {
 // @Param state query int false "状态" Enums(0, 1) default(1)
 // @Param page query int false "页码"
 // @Param page_size query int false "每页数量"
-// @Success 200 {object} model.Article "成功"
+// @Success 200 {object} model.ArticleSwagger "成功"
 // @Failure 400 {object} err_code.Error "请求错误"
 // @Failure 500 {object} err_code.Error "内部错误"
 // @Router /api/v1/articles [get]
@@ -41,7 +41,7 @@ func (a Article) List(c *gin.Context) {}
 // @Param cover_image_url body string false "封面图片地址" maxlength(255)
 // @Param state body int false "状态" Enums(0, 1) default(1)
 // @Param created_by body string true "创建者" minlength(3) maxlength(100)
-// @Success 200 {object} model.Article "成功"
+// @Success 200 {object} model.ArticleSwagger "成功"
 // @Failure 400 {object} err_code.Error "请求错误"
 // @Failure 500 {object} err_code.Error "内部错误"
 // @Router /api/v1/articles [post]
@@ -56,7 +56,7 @@ func (a Article) Create(c *gin.Context) {}
 // @Param cover_image_url body string false "封面图片地址" maxlength(255)
 // @Param state body int false "状态" Enums(0, 1) default(1)
 // @Param modified_by body string true "修改者" minlength(3) maxlength(100)
-// @Success 200 {array} model.Article "成功"
+// @Success 200 {array} model.ArticleSwagger "成功"
 // @Failure 400 {object} err_code.Error "请求错误"
 // @Failure 500 {object} err_code.Error "内部错误"
 // @Router /api/v1/articles/{id} [put]
