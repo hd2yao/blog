7e879b2af4bc81710916704aa3413bbf03d08224
hd2yao <1054290355@qq.com>
2023-06-27 15:53:22 +0800

    feat:add struct tag form and binding

--- internal/model/article.go
+++ internal/model/article.go
@@ -19,3 +19,33 @@ type ArticleSwagger struct {
     List  []*Article
     Pager *app.Pager
 }
+
+type CountArticleRequest struct {
+    Title string `json:"title" form:"title" binding:"max=100"`
+    State uint8  `json:"state" form:"state,default=1" binding:"oneof=0 1"`
+}
+
+type ArticleListRequest struct {
+    Title string `json:"title" form:"title" binding:"max=100"`
+    State uint8  `json:"state" form:"state,default=1" binding:"oneof=0 1"`
+}
+
+type CreateArticleRequest struct {
+    Title         string `json:"title" form:"title" binding:"max=100"`
+    Desc          string `json:"desc" form:"desc" binding:"required"`
+    Content       string `json:"content" form:"content" binding:"required,min=0"`
+    CoverImageUrl string `json:"cover_image_url" form:"cover_image_url" binding:"required,min=0"`
+    CreatedBy     string `json:"created_by" form:"created_by" binding:"required,min=3,max=100"`
+    State         uint8  `json:"state" form:"state,default=1" binding:"oneof=0 1"`
+}
+
+type UpdateArticleRequest struct {
+    ID         uint32 `json:"id" form:"id" binding:"required,gte=1"`
+    Title      string `json:"title" form:"title" binding:"max=100"`
+    State      uint8  `json:"state" form:"state,default=1" binding:"oneof=0 1"`
+    ModifiedBy string `json:"modified_by" form:"modified_by" binding:"required,min=3,max=100"`
+}
+
+type DeleteArticleRequest struct {
+    ID uint32 `json:"id" form:"id" binding:"required,gte=1"`
+}
--- internal/model/tag.go
+++ internal/model/tag.go
@@ -16,3 +16,30 @@ type TagSwagger struct {
     List  []*Tag
     Pager *app.Pager
 }
+
+type CountTagRequest struct {
+    Name  string `json:"name" form:"name" binding:"max=100"`
+    State uint8  `json:"state" form:"state,default=1" binding:"oneof=0 1"`
+}
+
+type TagListRequest struct {
+    Name  string `json:"name" form:"name" binding:"max=100"`
+    State uint8  `json:"state" form:"state,default=1" binding:"oneof=0 1"`
+}
+
+type CreateTagRequest struct {
+    Name      string `json:"name" form:"name" binding:"required,min=3,max=100"`
+    CreatedBy string `json:"created_by" form:"created_by" binding:"required,min=3,max=100"`
+    State     uint8  `json:"state" form:"state,default=1" binding:"oneof=0 1"`
+}
+
+type UpdateTagRequest struct {
+    ID         uint32 `json:"id" form:"id" binding:"required,gte=1"`
+    Name       string `json:"name" form:"name" binding:"min=3,max=100"`
+    State      uint8  `json:"state" form:"state" binding:"required,oneof=0 1"`
+    ModifiedBy string `json:"modified_by" form:"modified_by" binding:"required,min=3,max=100"`
+}
+
+type DeleteTagRequest struct {
+    ID uint32 `json:"id" form:"id" binding:"required,gte=1"`
+}
