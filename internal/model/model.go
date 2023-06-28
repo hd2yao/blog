95cacfccf8b658cdb54ea070d4cff890d06efc41
hd2yao <1054290355@qq.com>
2023-06-28 10:10:06 +0800

    feat:callback methods for public fields

--- internal/model/model.go
+++ internal/model/model.go
@@ -2,6 +2,7 @@ package model
 
 import (
     "fmt"
+    "time"
 
     "github.com/jinzhu/gorm"
     _ "github.com/jinzhu/gorm/dialects/mysql"
@@ -38,8 +39,89 @@ func NewDBEngine(databaseSetting *setting.DatabaseSettingS) (*gorm.DB, error) {
         db.LogMode(true)
     }
     db.SingularTable(true)
+    // 注册回调行为
+    db.Callback().Create().Replace("gorm:update_time_stamp", updateTimeStampForCreateCallback)
+    db.Callback().Update().Replace("gorm:update_time_stamp", updateTimeStampForUpdateCallback)
+    db.Callback().Delete().Replace("gorm:delete", deleteCallback)
+
     db.DB().SetMaxIdleConns(databaseSetting.MaxIdleConns)
     db.DB().SetMaxOpenConns(databaseSetting.MaxOpenConns)
 
     return db, nil
+}
+
+/*
+   model 的回调方法
+*/
+
+// 新增行为的回调
+func updateTimeStampForCreateCallback(scope *gorm.Scope) {
+    if !scope.HasError() {
+        nowTime := time.Now().Unix()
+        // 通过调用 scope.FieldByName 方法，获取当前是否包含所需的字段
+        if createTimeField, ok := scope.FieldByName("CreatedOn"); ok {
+            if createTimeField.IsBlank {
+                _ = createTimeField.Set(nowTime)
+            }
+        }
+
+        if modifyTimeField, ok := scope.FieldByName("ModifiedOn"); ok {
+            if modifyTimeField.IsBlank {
+                _ = modifyTimeField.Set(nowTime)
+            }
+        }
+    }
+}
+
+// 更新行为的回调
+func updateTimeStampForUpdateCallback(scope *gorm.Scope) {
+    // 获取当前设置了标识 gorm:update_column 的字段属性
+    if _, ok := scope.Get("gorm:update_column"); !ok {
+        // 若不存在，也就是没有自定义设置 update_column，
+        // 那么将会在更新回调内设置默认字段 ModifiedOn 的值为当前的时间戳
+        _ = scope.SetColumn("ModifiedOn", time.Now().Unix())
+    }
+}
+
+// 删除行为的回调
+func deleteCallback(scope *gorm.Scope) {
+    if !scope.HasError() {
+        var extraOption string
+        if str, ok := scope.Get("gorm:delete_option"); ok {
+            extraOption = fmt.Sprint(str)
+        }
+
+        // 判断是否存在 DeletedOn 和 IsDel 字段
+        deletedOnField, hasDeletedOnField := scope.FieldByName("DeletedOn")
+        isDelField, hasIdDelField := scope.FieldByName("IsDel")
+        // 若存在则调整为执行 UPDATE 操作进行软删除（修改 DeletedOn 和 IsDel 的值）
+        if !scope.Search.Unscoped && hasDeletedOnField && hasIdDelField {
+            now := time.Now().Unix()
+            scope.Raw(fmt.Sprintf(
+                "UPDATE %v SET %v=%v,%v=%v%v%v",
+                scope.QuotedTableName(),
+                scope.Quote(deletedOnField.DBName),
+                scope.AddToVars(now),
+                scope.Quote(isDelField.DBName),
+                scope.AddToVars(1),
+                // 调用 scope.CombinedConditionSql 方法完成 SQL 语句的组装
+                addExtraSpaceIfExist(scope.CombinedConditionSql()),
+                addExtraSpaceIfExist(extraOption),
+            )).Exec()
+        } else { // 否则执行 DELETE 进行硬删除
+            scope.Raw(fmt.Sprintf(
+                "DELETE FROM %v%v%v",
+                scope.QuotedTableName(),
+                addExtraSpaceIfExist(scope.CombinedConditionSql()),
+                addExtraSpaceIfExist(extraOption),
+            )).Exec()
+        }
+    }
+}
+
+func addExtraSpaceIfExist(str string) string {
+    if str != "" {
+        return " " + str
+    }
+    return ""
 }
